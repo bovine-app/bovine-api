beb0952da11ff259d286da0c4425fa39e43f35bd
Jesse B. Hannah <jesse@jbhannah.net>
2020-05-25 07:11:28 -0700

    CSRF protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
+# Base controller.
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  include ActionController::RequestForgeryProtection
+
+  protect_from_forgery with: :null_session
+
+  after_action :set_csrf_cookie, unless: -> { session.empty? }
+
+  protected
+
+  # Allow forgery protection unless it is explicitly disabled in the environment
+  # configuration.
+  def allow_forgery_protection
+    Rails.application.config.action_controller.allow_forgery_protection != false
+  end
+
+  private
+
+  def set_csrf_cookie
+    cookies[:csrf_token] = form_authenticity_token
+  end
 end
